6388a5e8ad4953ee89d638afa723645693750aad
Daniel Snider <danielsnider12@gmail.com>
2022-03-20 14:49:08 -0400

    full collection

--- experiments/mnist_batch_size/measure_batch.sh
+++ experiments/mnist_batch_size/measure_batch.sh
@@ -16,7 +16,7 @@ do
     mkdir -p $LOG_DIR/
 
     # Full data collection
-    NUM_TRIALS='3'
+    NUM_TRIALS='7'
     TARGET_VALUE='0.7'
 
     # Quick data collection
@@ -28,7 +28,7 @@ do
         echo "INPUT CONFIG: arch $architecture, batch $batch_size, lr $learning_rate";
         LOG_DIR_SUB_EXPERIMENT=$LOG_DIR/$architecture/$batch_size
 
-        EVAL_FREQUENCY_OVERRIDE=$(echo $(( step_to_threshold / 300 )))
+        EVAL_FREQUENCY_OVERRIDE=$(echo $(( step_to_threshold / 500 )))
         step_to_threshold_increased=$(echo $(( step_to_threshold + 58 )))
 
         EARLY_STOPPING_CONFIG='mnist_early_stopping_config.json'
@@ -102,23 +102,20 @@ done
 
 # rebuild best parameters csv
 echo "architecture,batch_size,trial_id,step_to_threshold,learning_rate,train.cross_entropy_error,train.classification_error,val.cross_entropy_error,val.classification_error,best_config_path" > out.csv
-for FILE in ./experiments/mnist_batch_size/logs*/**/trial_*/*.json
+for FILE in ./experiments/mnist_batch_size/logs*/**/trial_**/*.json
 do
-    STATUS=$(cat $FILE | jq -r '.status')
-    global_step=$(cat $FILE | jq -r '.global_step')
-    accuracy=$(cat $FILE | jq -r '.accuracy')
-    step_to_threshold=$(cat $FILE | jq -r '."extra.batch_science.step_to_threshold"')
-
-    architecture=$(cat $FILE | jq -r .'"extra.batch_science.architecture"')
-    batch_size=$(cat $FILE | jq -r .'"batch_size"')
-    trial_id=$(cat $FILE | jq -r .'"extra.batch_science.trial_id"')
-    step_to_threshold=$(cat $FILE | jq -r .'"global_step"')
-    learning_rate=$(cat $FILE | jq -r .'"extra.batch_science.learning_rate"')
-    train_cross_entropy_error=$(cat $FILE | jq -r .'"extra.batch_science.train_cross_entropy_error"')
-    train_classification_error=$(cat $FILE | jq -r .'"extra.batch_science.train_classification_error"')
-    val_cross_entropy_error=$(cat $FILE | jq -r .'"extra.batch_science.val_cross_entropy_error"')
-    val_classification_error=$(cat $FILE | jq -r .'"extra.batch_science.val_classification_error"')
-    best_config_path=$(cat $FILE | jq -r .'"extra.batch_science.best_config_path"')
+    JSON=$(cat $FILE)
+    global_step=$(echo $JSON | jq -r '.global_step')
+    architecture=$(echo $JSON | jq -r .'"extra.batch_science.architecture"')
+    batch_size=$(echo $JSON | jq -r .'"batch_size"')
+    trial_id=$(echo $JSON | jq -r .'"extra.batch_science.trial_id"')
+    step_to_threshold=$(echo $JSON | jq -r .'"global_step"')
+    learning_rate=$(echo $JSON | jq -r .'"extra.batch_science.learning_rate"')
+    train_cross_entropy_error=$(echo $JSON | jq -r .'"extra.batch_science.train_cross_entropy_error"')
+    train_classification_error=$(echo $JSON | jq -r .'"extra.batch_science.train_classification_error"')
+    val_cross_entropy_error=$(echo $JSON | jq -r .'"extra.batch_science.val_cross_entropy_error"')
+    val_classification_error=$(echo $JSON | jq -r .'"extra.batch_science.val_classification_error"')
+    best_config_path=$(echo $JSON | jq -r .'"extra.batch_science.best_config_path"')
 
     echo "$architecture,$batch_size,$trial_id,$step_to_threshold,$learning_rate,$train_cross_entropy_error,$train_classification_error,$val_cross_entropy_error,$val_classification_error,$best_config_path" | tee -a out.csv
 done
