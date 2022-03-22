a979b65dee78a986c5373d39bd5d2c712d2c51b4
Daniel Snider <danielsnider12@gmail.com>
2022-03-22 19:41:19 -0400

    normalize by steps at B=4

--- experiments/mnist_batch_size/plot_batch.py
+++ experiments/mnist_batch_size/plot_batch.py
@@ -10,9 +10,15 @@ import pandas as pd
 import seaborn as sns
 
 # Read Data
-input_file = '/home/dans/algorithmic-efficiency/best_parameters_full.csv'
 input_file = './out.csv'
+input_file = '/home/dans/algorithmic-efficiency/best_parameters_full.csv'
 df = pd.read_csv(input_file)
+
+
+for arch in df['architecture'].unique():
+    loc = df['architecture'] == arch
+    num_steps_at_min_batch = df[loc].step_to_threshold.iloc[0]
+    df.loc[loc, 'step_to_threshold'] = df.loc[loc, 'step_to_threshold'] / num_steps_at_min_batch
 
 # Plot
 sns.set_theme()
@@ -20,7 +26,7 @@ fig, ax = plt.subplots()
 sns.lineplot(data=df, ax=ax, x='batch_size', y='step_to_threshold', hue='architecture')
 
 # Style
-ax.set_ylabel('Steps')
+ax.set_ylabel('Steps / (# Steps at B=4)')
 ax.set_xlabel('Batch Size')
 plt.xscale('log', base=2)
 plt.yscale('log', base=2)
