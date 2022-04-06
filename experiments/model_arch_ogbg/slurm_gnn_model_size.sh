a184b918bf3720ca6a9f3b176ddaff04c8164d3a
Daniel Snider <danielsnider12@gmail.com>
2022-04-06 18:42:31 -0400

    longer sweep gnn size

--- experiments/model_arch_ogbg/slurm_gnn_model_size.sh
+++ experiments/model_arch_ogbg/slurm_gnn_model_size.sh
@@ -7,15 +7,17 @@
 #
 # wandb sweep /ssd003/home/dans/algorithmic-efficiency/experiments/model_arch_ogbg/sweep_gnn_model_size.yaml
 # # copy sweep ID
-# cd ~/slurm_logs
+# cd ~/slurm_logs-2
 # for run in {1..161}; do
 #   sbatch /ssd003/home/dans/algorithmic-efficiency/experiments/model_arch_ogbg/slurm_gnn_model_size.sh
 # done
-
+#
+# build 1 large JSON file
+# jq -s 'flatten' /h/dans/algorithmic-efficiency/logs/gnn_model_size_first_slurm/*/**/trial_**/*.json > slurm_gnn_size_merged.json
 
 #SBATCH --nodes 1
 #SBATCH --partition t4v2,rtx6000
-#SBATCH --time=2:30:00
+#SBATCH --time=4:00:00
 #SBATCH -c 4
 #SBATCH --mem=8GB
 #SBATCH --gres=gpu:4
@@ -31,4 +33,4 @@ module load jax0.2.24-cuda11.0-python3.8_jupyter
 source ~/venv2/bin/activate
 cd ~/algorithmic-efficiency
 wandb login ab8a559717268962dc374078301ec56912d60370
-exec wandb agent --count 1 danielsnider/mlc_held_out_gnn/htd79w5v # copy sweep ID here
+exec wandb agent --count 1 danielsnider/mlc_held_out_gnn/fzvw4nhj # copy sweep ID here
