11434cbb606c59d01c5e4667b7e6fe423f0d241e
Daniel Snider <danielsnider12@gmail.com>
2022-04-07 16:09:06 -0400

    wip

--- algorithmic_efficiency/wandb_utils.py
+++ algorithmic_efficiency/wandb_utils.py
@@ -1,23 +1,15 @@
 import signal
 import wandb
 
-from absl import flags
-from algorithmic_efficiency.logging_utils import _get_extra_metadata_as_dict
-
-FLAGS = flags.FLAGS
-
-
 def signal_handler(signum, frame):
   # https://docs.wandb.ai/guides/track/advanced/resuming#preemptible-sweeps
   wandb.mark_preempting()
 
-def setup(config=None):
+def setup(config=None, name=None):
   run = wandb.init(config=config, reinit=True)
   config = wandb.config
   signal.signal(signal.SIGTERM, signal_handler)
 
-  extra_metadata = _get_extra_metadata_as_dict(FLAGS.extra_metadata)
-  name = extra_metadata.get('wandb.name', None)
   if name:
     wandb.run.name = str(name)
     wandb.run.save()
