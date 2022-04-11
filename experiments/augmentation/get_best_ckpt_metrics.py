69ec00413a34501bf1f90a6df3abba37034007ab
Shawn Carere <shawncarere@gmail.com>
2022-04-11 13:48:58 -0400

    uploading first and best checkpoints for cifar baseline experiment

--- experiments/augmentation/get_best_ckpt_metrics.py
+++ experiments/augmentation/get_best_ckpt_metrics.py
@@ -9,11 +9,11 @@ trial_best = pd.DataFrame()
 num_trials = 5
 for i in range(1, num_trials + 1):
   best = df.loc[df['trial_idx'] == i]['loss'].idxmin()
-  trial_best = pd.concat([trial_best, df.iloc[[best]][['loss', 'accuracy', 'epoch', 'global_step']]])
+  trial_best = pd.concat([trial_best, df.iloc[[best]][['loss', 'accuracy', 'epoch', 'global_step', 'trial_idx']]])
 
 print('Loss: %.4f+-%.4f' % (np.mean(trial_best['loss']), np.std(trial_best['loss'])))
 print('Accuracy: %.4f+-%.4f' % (np.mean(trial_best['accuracy']), np.std(trial_best['accuracy'])))
 
 print('\nBest Checkpoints:')
 for i in range(num_trials):
-  print('Trial%i - step=%i, epoch=%i' % (i, trial_best['global_step'].values[i], trial_best['epoch'].values[i]))
+  print('Trial%i - step=%i, epoch=%i' % (trial_best['trial_idx'].values[i], trial_best['global_step'].values[i], trial_best['epoch'].values[i]))
