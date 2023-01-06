028ef87c375b03b7f164cb5d93160a7a100d9f7e
Daniel Snider <danielsnider12@gmail.com>
2023-01-06 17:05:53 -0500

    wip

--- algorithmic_efficiency/workloads/librispeech/download_data.sh
+++ algorithmic_efficiency/workloads/librispeech/download_data.sh
@@ -5,13 +5,13 @@ set -x
 for d in dev test; do
     for s in clean other; do
 	echo $d, $s
-	wget wget --progress=bar:force:noscroll http://www.openslr.org/resources/12/$d-$s.tar.gz
+	wget --quiet --progress=bar:force:noscroll http://www.openslr.org/resources/12/$d-$s.tar.gz
 	tar xzf $d-$s.tar.gz
     done
 done
 
-wget wget --progress=bar:force:noscroll http://www.openslr.org/resources/12/raw-metadata.tar.gz
-wget wget --progress=bar:force:noscroll http://www.openslr.org/resources/12/train-clean-100.tar.gz
+wget --quiet --progress=bar:force:noscroll http://www.openslr.org/resources/12/raw-metadata.tar.gz
+wget --quiet --progress=bar:force:noscroll http://www.openslr.org/resources/12/train-clean-100.tar.gz
 
 # Untar files
 tar xzf raw-metadata.tar.gz
