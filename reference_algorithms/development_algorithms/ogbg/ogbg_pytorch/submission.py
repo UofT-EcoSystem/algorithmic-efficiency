b9e2f8bf2c62663c8ac29180ef451dbec8a9579d
Daniel Snider <danielsnider12@gmail.com>
2023-01-10 22:04:38 +0000

    configurable gnn batch

--- reference_algorithms/development_algorithms/ogbg/ogbg_pytorch/submission.py
+++ reference_algorithms/development_algorithms/ogbg/ogbg_pytorch/submission.py
@@ -8,12 +8,20 @@ from algorithmic_efficiency import spec
 
 def get_batch_size(workload_name):
   # Return the global batch size.
+  batch_sizes = {'ogbg': 32768}  # default
+  batch_sizes = {'ogbg': 2048}  # largest possible on 2080
+
   quick_run = os.environ.get('HOTLINE_QUICK_RUN')
   if quick_run:
-    batch_sizes = {'ogbg': 2048}  # largest possible on 2080
     batch_sizes = {'ogbg': 64}  # smallest possible quick test
   else:
-    batch_sizes = {'ogbg': 32768}  # default
+    gpu_model = torch.cuda.get_device_name(0)
+    num_gpus = torch.cuda.device_count()
+    if 'V100-SXM2-16GB' in gpu_model:
+      batch_sizes = {'ogbg': min(4096 * num_gpus, 32768)}
+  from absl import logging
+  logging.info(f'\n\nbatch_sizes: {batch_sizes}\n')
+
   return batch_sizes[workload_name]
 
 
