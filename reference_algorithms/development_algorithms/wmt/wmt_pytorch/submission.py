b84d4d872f62d08886350c54254513ea39af1e17
Daniel Snider <danielsnider12@gmail.com>
2023-01-14 14:10:55 -0500

    wip

--- reference_algorithms/development_algorithms/wmt/wmt_pytorch/submission.py
+++ reference_algorithms/development_algorithms/wmt/wmt_pytorch/submission.py
@@ -20,7 +20,10 @@ def get_batch_size(workload_name):
     gpu_model = torch.cuda.get_device_name(0)
     num_gpus = torch.cuda.device_count()
     if 'V100-SXM2-16GB' in gpu_model:
-      batch_sizes = {'wmt': 16 * num_gpus }
+      batch_sizes = {'wmt': min(16 * num_gpus, 128) }
+    if '3090' in gpu_model:
+      batch_sizes = {'wmt': min(32 * num_gpus, 128) }
+
   logging.info(f'\n\nbatch_sizes: {batch_sizes}\n')
   return batch_sizes[workload_name]
 
