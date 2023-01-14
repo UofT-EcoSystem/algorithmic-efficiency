740ec85fb40133abd7096ead60a9b0ca02e3a12f
Daniel Snider <danielsnider12@gmail.com>
2023-01-14 14:54:57 -0500

    wip

--- reference_algorithms/development_algorithms/criteo1tb/criteo1tb_pytorch/submission.py
+++ reference_algorithms/development_algorithms/criteo1tb/criteo1tb_pytorch/submission.py
@@ -26,7 +26,9 @@ def get_batch_size(workload_name):
     gpu_model = torch.cuda.get_device_name(0)
     num_gpus = torch.cuda.device_count()
     if 'V100-SXM2-16GB' in gpu_model:
-      batch_sizes = {'criteo1tb': min(65_536 * num_gpus, default)}
+      batch_sizes = {'criteo1tb': min(131_072 * num_gpus, default)}
+    elif '3090' in gpu_model:
+      batch_sizes = {'criteo1tb': min(196_608  * num_gpus, default)}
   from absl import logging
   logging.info(f'\n\nbatch_sizes: {batch_sizes}\n')
 
