45ae47066909dab78c9fb2e656a2486e66f304bb
Daniel Snider <danielsnider12@gmail.com>
2023-03-28 18:15:28 -0400

    wip transformer refactor

--- algorithmic_efficiency/workloads/wmt/wmt_pytorch/models.py
+++ algorithmic_efficiency/workloads/wmt/wmt_pytorch/models.py
@@ -149,8 +149,8 @@ class Transformer(nn.Module):
 
     super().__init__()
 
-    self.pos_encoder = PositionalEncoding(d_model, dropout_rate)
     self.shared_embedding = nn.Embedding(ntoken, d_model)
+    self.pos_encoder = PositionalEncoding(d_model, dropout_rate)
     self.encoder = Encoder(d_model,
                            nhead,
                            d_hid,
@@ -235,6 +235,7 @@ class Encoder(nn.Module):
     self.nhead = nhead
     self.shared_embedding = None
     self.pos_encoder = None
+    encoder_norm = nn.LayerNorm(d_model, eps=layer_norm_eps)
     encoder_layer = TransformerEncoderLayer(
         d_model,
         nhead,
@@ -242,7 +243,6 @@ class Encoder(nn.Module):
         dropout_rate,
         attention_dropout_rate=attention_dropout_rate,
         layer_norm_eps=layer_norm_eps)
-    encoder_norm = nn.LayerNorm(d_model, eps=layer_norm_eps)
     self.encoder = nn.TransformerEncoder(encoder_layer, nlayers, encoder_norm)
     # self.encoder = nn.TransformerEncoder(encoder_layer, nlayers)
 
@@ -392,11 +392,237 @@ class PositionalEncoding(nn.Module):
       return self.dropout(x)
 
 
+
+def _get_activation_fn(activation: str) -> Callable[[Tensor], Tensor]:
+    if activation == "relu":
+        return F.relu
+    elif activation == "gelu":
+        return F.gelu
+
+    raise RuntimeError("activation should be relu/gelu, not {}".format(activation))
+
+
+class _TransformerEncoderLayer(nn.Module):
+    r"""TransformerEncoderLayer is made up of self-attn and feedforward network.
+    This standard encoder layer is based on the paper "Attention Is All You Need".
+    Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones, Aidan N Gomez,
+    Lukasz Kaiser, and Illia Polosukhin. 2017. Attention is all you need. In Advances in
+    Neural Information Processing Systems, pages 6000-6010. Users may modify or implement
+    in a different way during application.
+
+    Args:
+        d_model: the number of expected features in the input (required).
+        nhead: the number of heads in the multiheadattention models (required).
+        dim_feedforward: the dimension of the feedforward network model (default=2048).
+        dropout: the dropout value (default=0.1).
+        activation: the activation function of the intermediate layer, can be a string
+            ("relu" or "gelu") or a unary callable. Default: relu
+        layer_norm_eps: the eps value in layer normalization components (default=1e-5).
+        batch_first: If ``True``, then the input and output tensors are provided
+            as (batch, seq, feature). Default: ``False`` (seq, batch, feature).
+        norm_first: if ``True``, layer norm is done prior to attention and feedforward
+            operations, respectively. Otherwise it's done after. Default: ``False`` (after).
+
+    Examples::
+        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
+        >>> src = torch.rand(10, 32, 512)
+        >>> out = encoder_layer(src)
+
+    Alternatively, when ``batch_first`` is ``True``:
+        >>> encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8, batch_first=True)
+        >>> src = torch.rand(32, 10, 512)
+        >>> out = encoder_layer(src)
+
+    Fast path:
+        forward() will use a special optimized implementation if all of the following
+        conditions are met:
+
+        - Either autograd is disabled (using ``torch.inference_mode`` or ``torch.no_grad``) or no tensor
+          argument ``requires_grad``
+        - training is disabled (using ``.eval()``)
+        - batch_first is ``True`` and the input is batched (i.e., ``src.dim() == 3``)
+        - activation is one of: ``"relu"``, ``"gelu"``, ``torch.functional.relu``, or ``torch.functional.gelu``
+        - at most one of ``src_mask`` and ``src_key_padding_mask`` is passed
+        - if src is a `NestedTensor <https://pytorch.org/docs/stable/nested.html>`_, neither ``src_mask``
+          nor ``src_key_padding_mask`` is passed
+        - the two ``LayerNorm`` instances have a consistent ``eps`` value (this will naturally be the case
+          unless the caller has manually modified one without modifying the other)
+
+        If the optimized implementation is in use, a
+        `NestedTensor <https://pytorch.org/docs/stable/nested.html>`_ can be
+        passed for ``src`` to represent padding more efficiently than using a padding
+        mask. In this case, a `NestedTensor <https://pytorch.org/docs/stable/nested.html>`_ will be
+        returned, and an additional speedup proportional to the fraction of the input that
+        is padding can be expected.
+    """
+    __constants__ = ['batch_first', 'norm_first']
+
+    def __init__(self, d_model: int, nhead: int, dim_feedforward: int = 2048, dropout: float = 0.1,
+                 activation: Union[str, Callable[[Tensor], Tensor]] = F.relu,
+                 layer_norm_eps: float = 1e-5, batch_first: bool = False, norm_first: bool = False,
+                 device=None, dtype=None) -> None:
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super(_TransformerEncoderLayer, self).__init__()
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout, batch_first=batch_first,
+                                            **factory_kwargs)
+        # Implementation of Feedforward model
+        self.linear1 = nn.Linear(d_model, dim_feedforward, **factory_kwargs)
+        self.activation = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model, **factory_kwargs)
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.norm_first = norm_first
+        # self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        # self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+        # self.dropout1 = nn.Dropout(dropout)
+
+        # Legacy string support for activation function.
+        if isinstance(activation, str):
+            activation = _get_activation_fn(activation)
+
+        # # We can't test self.activation in forward() in TorchScript,
+        # # so stash some information about it instead.
+        # if activation is F.relu or isinstance(activation, torch.nn.ReLU):
+        #     self.activation_relu_or_gelu = 1
+        # elif activation is F.gelu or isinstance(activation, torch.nn.GELU):
+        #     self.activation_relu_or_gelu = 2
+        # else:
+        #     self.activation_relu_or_gelu = 0
+        # self.activation = activation
+
+    def __setstate__(self, state):
+        super(_TransformerEncoderLayer, self).__setstate__(state)
+        if not hasattr(self, 'activation'):
+            self.activation = F.relu
+
+
+    def forward(self, src: Tensor, src_mask: Optional[Tensor] = None,
+                src_key_padding_mask: Optional[Tensor] = None) -> Tensor:
+        r"""Pass the input through the encoder layer.
+
+        Args:
+            src: the sequence to the encoder layer (required).
+            src_mask: the mask for the src sequence (optional).
+            src_key_padding_mask: the mask for the src keys per batch (optional).
+
+        Shape:
+            see the docs in Transformer class.
+        """
+
+        if src_key_padding_mask is not None:
+            _skpm_dtype = src_key_padding_mask.dtype
+            if _skpm_dtype != torch.bool and not torch.is_floating_point(src_key_padding_mask):
+                raise AssertionError(
+                    "only bool and floating types of key_padding_mask are supported")
+        # see Fig. 1 of https://arxiv.org/pdf/2002.04745v1.pdf
+        why_not_sparsity_fast_path = ''
+        if not src.dim() == 3:
+            why_not_sparsity_fast_path = f"input not batched; expected src.dim() of 3 but got {src.dim()}"
+        elif self.training:
+            why_not_sparsity_fast_path = "training is enabled"
+        elif not self.self_attn.batch_first :
+            why_not_sparsity_fast_path = "self_attn.batch_first was not True"
+        elif not self.self_attn._qkv_same_embed_dim :
+            why_not_sparsity_fast_path = "self_attn._qkv_same_embed_dim was not True"
+        elif not self.activation_relu_or_gelu:
+            why_not_sparsity_fast_path = "activation_relu_or_gelu was not True"
+        elif not (self.norm1.eps == self.norm2.eps):
+            why_not_sparsity_fast_path = "norm1.eps is not equal to norm2.eps"
+        elif src_mask is not None:
+            why_not_sparsity_fast_path = "src_mask is not supported for fastpath"
+        elif src.is_nested and src_key_padding_mask is not None:
+            why_not_sparsity_fast_path = "src_key_padding_mask is not supported with NestedTensor input for fastpath"
+        elif self.self_attn.num_heads % 2 == 1:
+            why_not_sparsity_fast_path = "num_head is odd"
+        elif torch.is_autocast_enabled():
+            why_not_sparsity_fast_path = "autocast is enabled"
+
+        if not why_not_sparsity_fast_path:
+            tensor_args = (
+                src,
+                self.self_attn.in_proj_weight,
+                self.self_attn.in_proj_bias,
+                self.self_attn.out_proj.weight,
+                self.self_attn.out_proj.bias,
+                self.norm1.weight,
+                self.norm1.bias,
+                self.norm2.weight,
+                self.norm2.bias,
+                self.linear1.weight,
+                self.linear1.bias,
+                self.linear2.weight,
+                self.linear2.bias,
+            )
+
+            # We have to use list comprehensions below because TorchScript does not support
+            # generator expressions.
+            if torch.overrides.has_torch_function(tensor_args):
+                why_not_sparsity_fast_path = "some Tensor argument has_torch_function"
+            elif not all((x.is_cuda or 'cpu' in str(x.device)) for x in tensor_args):
+                why_not_sparsity_fast_path = "some Tensor argument is neither CUDA nor CPU"
+            elif torch.is_grad_enabled() and any(x.requires_grad for x in tensor_args):
+                why_not_sparsity_fast_path = ("grad is enabled and at least one of query or the "
+                                              "input/output projection weights or biases requires_grad")
+
+            if not why_not_sparsity_fast_path:
+                return torch._transformer_encoder_layer_fwd(
+                    src,
+                    self.self_attn.embed_dim,
+                    self.self_attn.num_heads,
+                    self.self_attn.in_proj_weight,
+                    self.self_attn.in_proj_bias,
+                    self.self_attn.out_proj.weight,
+                    self.self_attn.out_proj.bias,
+                    self.activation_relu_or_gelu == 2,
+                    self.norm_first,
+                    self.norm1.eps,
+                    self.norm1.weight,
+                    self.norm1.bias,
+                    self.norm2.weight,
+                    self.norm2.bias,
+                    self.linear1.weight,
+                    self.linear1.bias,
+                    self.linear2.weight,
+                    self.linear2.bias,
+                    # TODO: if src_mask and src_key_padding_mask merge to single 4-dim mask
+                    src_mask if src_mask is not None else src_key_padding_mask,
+                    1 if src_key_padding_mask is not None else
+                    0 if src_mask is not None else
+                    None,
+                )
+
+
+        x = src
+        if self.norm_first:
+            x = x + self._sa_block(self.norm1(x), src_mask, src_key_padding_mask)
+            x = x + self._ff_block(self.norm2(x))
+        else:
+            x = self.norm1(x + self._sa_block(x, src_mask, src_key_padding_mask))
+            x = self.norm2(x + self._ff_block(x))
+
+        return x
+
+    # self-attention block
+    def _sa_block(self, x: Tensor,
+                  attn_mask: Optional[Tensor], key_padding_mask: Optional[Tensor]) -> Tensor:
+        x = self.self_attn(x, x, x,
+                           attn_mask=attn_mask,
+                           key_padding_mask=key_padding_mask,
+                           need_weights=False)[0]
+        return self.dropout1(x)
+
+    # feed forward block
+    def _ff_block(self, x: Tensor) -> Tensor:
+        x = self.linear2(self.dropout(self.activation(self.linear1(x))))
+        return self.dropout2(x)
+
+
 # TransformerEncoderLayer and TransformerDecoderLayer are taken from:
 # https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/transformer.py
 # Only difference is using custom MultiheadAttention modules without bias and
 # '_qkv_same_embed_dim' always set to 'False'.
-class TransformerEncoderLayer(nn.TransformerEncoderLayer):
+class TransformerEncoderLayer(nn.Module):
   r"""TransformerEncoderLayer is made up of self-attn and feedforward network.
   This standard encoder layer is based on the paper "Attention Is All You Need".
   Ashish Vaswani, Noam Shazeer, Niki Parmar, Jakob Uszkoreit, Llion Jones,
@@ -444,17 +670,8 @@ class TransformerEncoderLayer(nn.TransformerEncoderLayer):
                device=None,
                dtype=None) -> None:
     factory_kwargs = {'device': device, 'dtype': dtype}
-    super().__init__(
-        d_model,
-        nhead,
-        dim_feedforward=dim_feedforward,
-        dropout=dropout_rate,
-        activation=activation,
-        layer_norm_eps=layer_norm_eps,
-        batch_first=batch_first,
-        norm_first=norm_first,
-        device=device,
-        dtype=dtype)
+    super().__init__()
+    self.norm1 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
     self.self_attn = MultiheadAttention(
         d_model,
         nhead,
@@ -462,6 +679,17 @@ class TransformerEncoderLayer(nn.TransformerEncoderLayer):
         batch_first=batch_first,
         bias=False,
         **factory_kwargs)
+    self.dropout1 = nn.Dropout(dropout_rate)
+    self.norm2 = nn.LayerNorm(d_model, eps=layer_norm_eps, **factory_kwargs)
+    self.feed_forward = FeedForward(
+        d_model,
+        nhead,
+        dropout_rate=dropout_rate,
+        dim_feedforward=dim_feedforward,
+        batch_first=batch_first,
+        bias=False,
+         **factory_kwargs
+    )
 
   def forward(self, src: Tensor, src_mask: Optional[Tensor] = None, src_key_padding_mask: Optional[Tensor] = None) -> Tensor:
       r"""Pass the input through the encoder layer.
@@ -479,20 +707,12 @@ class TransformerEncoderLayer(nn.TransformerEncoderLayer):
 
       # with hotline.annotate('TransformerEncoderLayer'):
       x = src
-      if self.norm_first:
-        # with hotline.annotate('LayerNorm'):
-          xx = self.norm1(x)
-          x = x + self._sa_block(xx, src_mask, src_key_padding_mask)
-        # with hotline.annotate('LayerNorm'):
-          xx = self.norm2(x)
-          x = x + self._ff_block(xx)
-      else:
-          x = x + self._sa_block(x, src_mask, src_key_padding_mask)
-        # with hotline.annotate('LayerNorm'):
-          x = self.norm1(x)
-          x = x + self._ff_block(x)
-        # with hotline.annotate('LayerNorm'):
-          x = self.norm2(x)
+    # with hotline.annotate('LayerNorm'):
+      xx = self.norm1(x)
+      x = x + self._sa_block(xx, src_mask, src_key_padding_mask)
+    # with hotline.annotate('LayerNorm'):
+      xx = self.norm2(x)
+      x = x + self.feed_forward(xx)
 
       return x
 
@@ -521,6 +741,40 @@ class TransformerEncoderLayer(nn.TransformerEncoderLayer):
       # with hotline.annotate('dropout'):
         x = self.dropout2(x)
         return x
+
+
+class FeedForward(nn.Module):
+    def __init__(
+          self,
+          d_model,
+          nhead,
+          dropout_rate,
+          dim_feedforward,
+          batch_first,
+          bias,
+           **factory_kwargs
+        ):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, dim_feedforward, **factory_kwargs)
+        self.activation = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.linear2 = nn.Linear(dim_feedforward, d_model, **factory_kwargs)
+        self.dropout2 = nn.Dropout(dropout_rate)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.linear1(x)
+      # with hotline.annotate('relu'):
+        x = self.activation(x)
+      # with hotline.annotate('dropout'):
+        x = self.dropout(x)
+      # with hotline.annotate('linear'):
+        x = self.linear2(x)
+      # with hotline.annotate('dropout'):
+        x = self.dropout2(x)
+        return x
+
+
+
 
 # Modified to use cache for autoregressive decoding.
 class TransformerDecoder(nn.Module):
